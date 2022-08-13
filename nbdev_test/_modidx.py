268e90cfdd92f385d027b1c8162e76ee7ca1b2ab
jamesAmckinnon <james.mckinnon95@gmail.com>
2022-08-13 15:52:35 -0600

    tutorial stuff

--- nbdev_test/_modidx.py
+++ nbdev_test/_modidx.py
@@ -32,4 +32,5 @@ d = { 'settings': { 'allowed_cell_metadata_keys': '',
                 'tst_flags': 'notest',
                 'user': 'jamesAmckinnon',
                 'version': '0.0.1'},
-  'syms': {'nbdev_test\\core': {'nbdev_test\\core.foo': 'https://jamesAmckinnon.github.io/nbdev_test/nbdev_test\\core.html#foo'}}}
+  'syms': { 'nbdev_test\\core': { 'nbdev_test\\core.foo': 'https://jamesAmckinnon.github.io/nbdev_test/nbdev_test\\core.html#foo',
+                                  'nbdev_test\\core.say_hello': 'https://jamesAmckinnon.github.io/nbdev_test/nbdev_test\\core.html#say_hello'}}}
